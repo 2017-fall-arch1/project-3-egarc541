db2048d7eaacae25e25b664a0db2ad7de282752d
student <student@localhost.localdomain>
2017-12-07 11:13:27 -0700

    Assembly Added

---
+++ Project3/wdt_handler2.s
@@ -0,0 +1,103 @@
+	.file	"_wdt_handler.c"
+.text
+	.balign 2
+	.global	WDT
+	.section	__interrupt_vector_11,"ax",@progbits
+	.word	WDT
+	.text
+	
+
+	.extern redrawScreen
+	.extern wdt_c_handler
+WDT:
+; start of function
+; attributes: interrupt 
+; framesize_regs:     24
+; framesize_locals:   0
+; framesize_outgoing: 0
+; framesize:          24
+; elim ap -> fp       26
+; elim fp -> sp       0
+; saved regs: R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15
+	; start of prologue
+	PUSH	R15
+	PUSH	R14
+	PUSH	R13
+	PUSH	R12
+	PUSH	R11
+	PUSH	R10
+	PUSH	R9
+	PUSH	R8
+	PUSH	R7
+	PUSH	R6
+	PUSH	R5
+	PUSH	R4
+	; end of prologue
+wdt_c_handler.s: sub #6, 0(r1)
+	MOV #0, r12
+	BIS sr, &P1OUT
+	ADD #1, r13
+
+loop:
+	CMP #15, r13
+	JNE out
+	MOV &mball, r12
+	MOV &leftP, r13
+	MOV &rightP,r14
+	MOV &&fieldFence, r15
+	CALL #mlAdvance
+	CALL #p2sw_read
+	ADD r14, 0(r1)
+	CMP 0(r1), &S1
+	JNE else
+	ADD #3, 0(r13)
+	JMP update
+	
+	CMP 0(r1), &S2
+	JNE else
+	SUB #3, 0(r13)
+	JMP update
+
+	CMP 0(r1), &S3
+	JNE else
+	ADD #3, 0(r14)
+	JMP update
+
+	CMP 0(r1), &S4
+	JNE else
+	SUB #3, 0(r14)
+	JMP update
+
+else:
+	MOV #0, 0(r13)
+	MOV #0, 0(r14)
+	JMP loop
+
+done:
+	BIC sr
+	AND sr, &P1OUT
+	ADD #6, r1
+	POP r0
+	 
+	; start of epilogue
+	POP	R4
+	POP	R5
+	POP	R6
+	POP	R7
+	POP	R8
+	POP	R9
+	POP	R10
+	POP	R11
+	POP	R12
+	POP	R13
+	POP	R14
+	POP	R15
+	cmp	#0, &redrawScreen
+	jz	ball_no_move
+	and	#0xffef, 0(r1)	; clear CPU off in saved SR
+ball_no_move:	
+	RETI
+	.size	WDT, .-WDT
+	.local	count
+	.comm	count,1,1
+	.ident	"GCC: (GNU) 4.9.1 20140707 (prerelease (msp430-14r1-364)) (GNUPro 14r1) (Based on: GCC 4.8 GDB 7.7 Binutils 2.24 Newlib 2.1)"
